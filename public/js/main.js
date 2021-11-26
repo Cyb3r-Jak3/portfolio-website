0118a6411c17b22b376d2a3d6c83083604b6d5b2
Jacob White <jake@jwhite.network>
2021-11-26 12:54:06 -0500

    USE X-API-LOCATION header

--- public/js/main.js
+++ public/js/main.js
@@ -246,6 +246,7 @@ $("#resume_form").submit(function downloadFile(evt) {
 	evt.preventDefault();
 	var formData = new FormData($(this)[0]);
 	var req = new XMLHttpRequest();
+	req.setRequestHeader("X-API-LOCATION", window.location.hostname)
 	req.open("POST", "https://api.cyberjake.xyz/encrypted_resume", true);
 	req.responseType = "blob";
 	req.onload = function (event) {
